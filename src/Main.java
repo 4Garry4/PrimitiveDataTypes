bb0f4b5b68f1ad9d690ab29c11d8247c23d507d7
4garry4 <regionhia@gmail.com>
2023-10-11 15:39:31 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 7500; //стоимость билета
+        int Miles = 1;// количество бонусов
+        int i = 20; //каждые 20 рублей стоимости авиабилета
+
+
+        System.out.println("количество бонусных миль");
+        System.out.println(ticket/i);
+
+
+    }
+}
